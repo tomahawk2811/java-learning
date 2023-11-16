564b21dca3724f222e8f7597e285beb8807df6dc
tomahawk28 <tomahawk280111@gmail.com>
2023-11-16 14:11:30 +0800

    Now in simple programs

---
+++ Simple Programs/WelcomeWithThreeMessages.java
@@ -0,0 +1,9 @@
+package SimplePrograms;
+public class WelcomeWithThreeMessages {
+	public static void main(String[] args) {
+		System.out.println("Programming is Fun");
+		System.out.println("Fundementals requium");
+		System.out.println("Problem solving one bit at a time");
+	  }
+
+ } 
--- WelcomeWithThreeMessages.java
+++
@@ -1,9 +0,0 @@
-package SimplePrograms;
-public class WelcomeWithThreeMessages {
-	public static void main(String[] args) {
-		System.out.println("Programming is Fun");
-		System.out.println("Fundementals requium");
-		System.out.println("Problem solving one bit at a time");
-	  }
-
- } 
